0f3d1f60b91ce08c5ad9faa949a55d305d8cff83
AyandaDladla <115835442+AyandaDladla@users.noreply.github.com>
2026-04-19 14:12:07 +0200

    - mobile fix

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,7 +1,20 @@
 import buildConfig from './rollup.config.js';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
 
 // use first output of first config block for dev
 const config = Array.isArray(buildConfig) ? buildConfig[0] : buildConfig;
 Array.isArray(config.output) && (config.output = config.output[0]);
+
+config.plugins = [
+    ...config.plugins,
+    serve({
+        open: true,
+        openPage: '/example/weather-timezone/',
+        contentBase: '.',
+        port: 3000
+    }),
+    livereload('dist')
+];
 
 export default config;
